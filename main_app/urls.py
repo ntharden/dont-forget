aaacadec8bca88780b28512fb28a6b264f01231e
nathanth <nathan.hardenburg@icloud.com>
2022-08-08 17:40:36 -0500

    renders topics

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
-  path('topics/', views.topics_index, name='topics'),
+  path('topics/', views.topics_index, name='topics_index'),
+  path('topics/<int:topic_id>', views.topics_detail, name='topics_detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,23 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Topic
 
 # Create your views here.
-class Topic:
-	def __init__(self, name, description):
-		self.name = name
-		self.description = description
-
-topics = [
-	Topic('Template Literals', 'Template literals are part of the ES2015 specification. They are another way to define and use strings in JavaScript.'),
-	Topic('Functional Programming', 'Functional Programming (FP) is the process of building software by composing pure functions, avoiding shared state, mutable data, and side effects. FP is also declarative rather than imperative, and application state flows through pure functions. With Object Oriented Programming (OOP), application state is usually shared and co-located with methods in objects.'),
-	Topic('MUI & React Forms', 'The React UI library you always wanted.'),
-]
 
 def home(request):
-  return HttpResponse('<h1>Hello</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def topics_index(request):
-  return render(request, 'topics/index.html', { 'topics':topics })
+	topics = Topic.objects.all()
+	return render(request, 'topics/index.html', { 'topics':topics })
+
+def topics_detail(request, topic_id):
+	topic = Topic.objects.get(id=topic_id)
+	return render(request, 'topics/detail.html', { 'topic': topic })
