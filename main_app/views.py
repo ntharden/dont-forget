2be08b2ab862f149dcdb1882e789675efcda3551
nathanth <nathan.hardenburg@icloud.com>
2022-08-09 21:17:50 -0500

    redirects after successful create

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from .models import Topic
+from django.urls import reverse
 
 # Create your views here.
 
@@ -21,3 +22,10 @@ def topics_detail(request, topic_id):
 class TopicCreate(CreateView):
 	model = Topic
 	fields = '__all__'
+	success_url = '/topics/'
+
+	def __str__(self):
+		return self.template_name
+		
+	def get_absolute_url(self):
+		return reverse('topics_detail', kwargs={'topic_id': self.id})
