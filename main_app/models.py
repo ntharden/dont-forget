cd2bf8ad5d827338b1f4680df71017ea11465c43
nathanth <nathan.hardenburg@icloud.com>
2022-08-09 22:32:06 -0500

    figuring out adding fields to model; stopping point

--- main_app/models.py
+++ main_app/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Topic(models.Model):
 	name = models.CharField(max_length=50)
 	description = models.TextField(max_length=300)
+	category = models.CharField(max_length=50)
+	notion_url = models.CharField(max_length=50)
 
 	def __str__(self):
 		return self.name
