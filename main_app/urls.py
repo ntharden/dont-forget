85e76e61c0920fd7c5fa21bd05a33b5f78986945
nathanth <nathan.hardenburg@icloud.com>
2022-08-11 18:17:19 -0500

    adds clickable links for urls in details page

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,7 +5,9 @@ urlpatterns = [
   path('', views.Home.as_view(), name='home'),
   path('about/', views.about, name='about'),
   path('topics/', views.topics_index, name='topics_index'),
-  path('topics/<int:topic_id>', views.topics_detail, name='topics_detail'),
+  path('topics/<int:topic_id>/', views.topics_detail, name='topics_detail'),
   path('topics/create/', views.TopicCreate.as_view(), name='topics_create'),
+  path('topics/<int:pk>/update/', views.TopicUpdate.as_view(), name='topics_update'),
+  path('topics/<int:pk>/delete/', views.TopicDelete.as_view(), name='topics_delete'),
   path('accounts/signup/', views.signup, name='signup'),
 ]
